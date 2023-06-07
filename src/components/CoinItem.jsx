da4252a0961f132a4d48b689805c4d0de9c59754
Ilias Dinia <iliasd752@gmail.com>
2023-06-07 13:53:33 +0300

    Ternary operator in order to display positive/negative price changes with their appropriate color

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -3,20 +3,25 @@ import React from "react";
 
 const CoinItem = ({coin}) => {
     return (
-        <tr>
-            <td><AiOutlineStar /></td>
+        <tr className='h-[80px] border-b overflow-hidden'>
+            <td><AiOutlineStar/></td>
             <td>{coin.market_cap_rank}</td>
             <td>
-                <div>
-                    <img src={coin.image} alt={coin.id}/>
-                    <p>{coin.name}</p>
+                <div className='flex items-center'>
+                    <img className='w-6 mr-2 rounded-full' src={coin.image} alt={coin.id}/>
+                    <p className='hidden sm:table-cell'>{coin.name}</p>
                 </div>
             </td>
             <td>{coin.symbol}</td>
             <td>{coin.current_price}</td>
-            <td>{coin.price_change_percentage_24h}</td>
-            <td>{coin.total_volume}</td>
-            <td>{coin.market_cap}</td>
+            <td>
+                {coin.price_change_percentage_24h > 0 ? (
+                    <p className='text-green-600'>{coin.price_change_percentage_24h}</p>) : (
+                    <p className='text-red-600'>{coin.price_change_percentage_24h}</p>)}
+
+            </td>
+            <td className='w-[180px] hidden md:table-cell'>{coin.total_volume}</td>
+            <td className='w-[180px] hidden sm:table-cell'>{coin.market_cap}</td>
             <td>
 
             </td>
